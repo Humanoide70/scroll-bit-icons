92f383e528f77982ccfcab6f78fa5884e85bd266
Humanoide70 <humanoide70@gmail.com>
2021-07-15 15:23:44 +0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+scrollbit.setIcon(
+IconNames.Heart,
+0,
+1,
+128
+)
+scrollbit.setIcon(
+IconNames.Heart,
+6,
+1,
+128
+)
+scrollbit.setIcon(
+IconNames.Heart,
+12,
+1,
+128
+)
+scrollbit.show()
+basic.forever(function () {
+	
+})
